cdde233cff30866c8445504c2673025fb079a2a1
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-08-20 17:50:15 +0530

    Project Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-300 flex items-center justify-center min-h-screen'>
+      <h1 className='bg-purple-400 w-72 h-72 flex items-center justify-center rounded-b-full font-extrabold text-4xl'>
+        Kaise ho aap 
+      </h1>
+    </div>
+  )
+}
+
+export default App
