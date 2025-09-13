c1c18d952232a382169ba16138d8cac4214e0bff
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-09-13 19:08:50 +0530

    13:server-setup & database setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 import express from "express";
 
-import bodyParser from "body-parser";
-
-import mongoose from "mongoose";
-
 import cors from "cors";
+
+import "dotenv/config"; // todo=> Express mein hum log extra package install karte hai env file ke liye,But next js mein aisa kuch nhi hota hai ,aur dekha jaye to isi choti-choti chijo ke wajah se aaj kaal modern dev mein next js use ho raha hai.
+import connectDb from "./config/mongodb.js";
+
+const app = express(); 
+
+// Connecting Our Database :-
+
+await connectDb();
+
+app.use(cors()); //! Basically sabko pta hai ki hum cors ko isiliye use krte hai kyuki hum apne backend ko kisi bhi type ke website ke domain se connect kr sekte hai ,aur yedi hum ye nhi krte hai to "CORS ERROR ata hai "
+
+app.get("/", (req, res) => res.send("Api Is Working"));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server Is Ruining on Port is :- ${PORT}`);
+});
