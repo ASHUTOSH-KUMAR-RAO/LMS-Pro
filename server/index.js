3f03be7fe69beca012e7fafaa2003d12d0b57155
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-09-15 21:28:41 +0530

    17:implemented-educator-api-endpoint

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import { clerkWebhooks } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
 import connectCloudinary from "./config/cloudinary.js";
+import courseRouter from "./routes/courseRoute.js";
 
 const app = express();
 
@@ -27,6 +28,8 @@ app.get("/", (req, res) => res.send("Api Is Working"));
 app.post("/clerk", express.json(), clerkWebhooks);
 
 app.use("/api/educator",express.json(),educatorRouter)
+
+app.use("/api/course",express.json(),courseRouter)
 
 const PORT = process.env.PORT || 4000;
 
